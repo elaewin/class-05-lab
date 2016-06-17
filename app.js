2494390f4448846eb00ec50c62879734ef858964
Erica Winberry <elaewin@gmail.com>
2016-06-17 13:49:15 -0700

    making sure branch is clean before starting next project.

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 /////////////////////////////////////
 /* Problem 1 (this is your demo)
 Write a function called sum() that takes in two numbers as arguments and then returns an array where the first element is the sum of those numbers, and the second element is a string that EXACTLY follows this example and uses the values that were input into the function:
-
 "The sum of 4 and 7 is 11."
-
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSum() function below and check the console to see if the test passes.*/
 
 // Write your code here
 function sum(a,b){
-  var addUp = a + b;
-  var message = 'The sum of ' + a + ' and ' + b + ' is ' + addUp + '.';
-  return [addUp, message];
+  var result = a + b;
+  var message = 'The sum of ' + a + ' and ' + b + ' is ' + result + '.';
+  // console.log(message);
+  // console.log('The sum of 4 and 7 is 11.');
+  return [result, message];
 }
 
 // Here is the test for sum(); uncomment it to run it
@@ -21,9 +21,7 @@ testSum(4, 7);
 /////////////////////////////////////
 /* Problem 2
 Write a function called multiply() that takes in two numbers as arguments and returns an array where the first element is the product of those numbers, and the second element is a string that EXACTLY follows this example and uses the values that were input into the function:
-
 "The product of 4 and 7 is 28."
-
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiply() function and see if the test passes.*/
 
 // Write your code here
@@ -39,12 +37,9 @@ function multiply(a,b){
 /////////////////////////////////////
 /* Problem 3
 Write a function called sumAndMultiply() that takes in three numbers as arguments and returns an array where the first element is the sum of those three numbers, the second element is the product of those three numbers,  and the third and fourth elements are strings that EXACTLY follow this example and use the values that were input into the function:
-
 Third element: "4 and 7 and 5 sum to 16."
 Fourth element: "The product of 4 and 7 and 5 is 140."
-
 Note: You are not allowed to use the arithmetic operators + and * in this function. Instead, you must use your existing sum() and multiply() functions to do the calculations.
-
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiply() function and see if the test passes.*/
 
 // Write your code here
@@ -60,11 +55,8 @@ function sumAndMultiply(a,b,c){
 /////////////////////////////////////
 /* Problem 4
 Write a function called sumArray() that takes in an array of numbers as its argument and then returns an array where the first element is the sum of the numbers in the array, and the second element is a string that EXACTLY follows this example and uses the values that were input into the function:
-
 "2,3,4 was passed in as an array of numbers, and 9 is their sum."
-
 Note: You are not allowed to use the arithmetic operators + and * in this function. Instead, you must use your existing sum() and multiply() functions to do the calculations.
-
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumArray() function and see if the test passes.*/
 
 // Write your code here
@@ -82,11 +74,8 @@ function sumArray(testArray){
 /////////////////////////////////////
 /* Problem 5
 Write a function called multiplyArray() that takes an array of numbers as its argument and returns an array whose first element is the product of those numbers, and the second element is a string that EXACTLY follows this example and uses the values that were input into the function:
-
 "The numbers 2,3,4 have a product of 24."
-
 Note: You are not allowed to use the arithmetic operators + and * in this function. Instead, you must use your existing sum() and multiply() functions to do the calculations.
-
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiplyArray() function and see if the test passes.*/
 
 // Write your code here
